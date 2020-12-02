3c8b949d8dc8fd23df0ad3c06b6f04ba13a85e0d
wjxchn <1822630963@qq.com>
2020-12-02 15:46:10 +0800

    v0.03:实现了swagger

--- jingzhou/src/main/java/jingzhou/Controller/UserController.java
+++ jingzhou/src/main/java/jingzhou/Controller/UserController.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.*;
+
 import java.util.Date;
 
+@Api(value = "用户控制类")
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("findusername/{id}")
+    @ApiOperation(value = "获取用户", notes = "根据用户ID获取用户注册时间")
+    @GetMapping("find_user_register_time/{id}")
     public String findUserById(@PathVariable("id") Integer id){
         return userRepository.findUserById((int)id).getTime().toString();
     }
