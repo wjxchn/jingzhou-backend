f8721a275de5b2aa9164a1a44a4d03dd7ec002e0
NorthanX <786260621@qq.com>
2020-12-16 21:37:58 +0800

    更新部分热点和门户controller

--- jingzhou/src/main/java/jingzhou/Controller/GovernController.java
+++ jingzhou/src/main/java/jingzhou/Controller/GovernController.java
@@ -3,6 +3,9 @@ package jingzhou.Controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import jingzhou.MySQLTable.AuthUser;
+import jingzhou.POJO.Result;
+import jingzhou.Service.AuthUserService;
+import jingzhou.Service.InstitutionService;
 import jingzhou.repository.AuthUserRepository;
 import jingzhou.repository.InstitutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,47 +19,49 @@ import java.util.Map;
 @Api(value = "门户与学术管理子系统")
 public class GovernController {
     @Autowired
-    AuthUserRepository authUserRepository;
+    AuthUserService authUserService;
 
     @Autowired
-    InstitutionRepository institutionRepository;
+    InstitutionService institutionService;
 
     @ApiOperation(value = "认领门户接口")
     @PostMapping("claimportal")
-    public Map<String, Object> claimportal(@RequestParam("username") String username,@RequestParam("institutionname") String institutionname, @RequestParam("researchfield") String researchfield, @RequestParam("realname") String realname){
-        HashMap<String, Object> result = new HashMap<>();
+    public Result claimportal(@RequestParam("username") String username,@RequestParam("institutionname") String institutionname, @RequestParam("researchfield") String researchfield, @RequestParam("realname") String realname){
+
         AuthUser authUser = new AuthUser();
         authUser.setUsername(username);
-        authUser.setInstitutionid(institutionRepository.findInstitutionByInstitutionname(institutionname).getInstitutionid());
+        authUser.setInstitutionid(institutionService.getByName(institutionname).getInstitutionid());
         authUser.setResearchfield(researchfield);
         authUser.setRealname(realname);
-        authUserRepository.save(authUser);
-        result.put("code",200);
-        result.put("msg","认领门户成功");
+
+        Result result = new Result("获取信息成功", 200);
+        result.getData().put("authUser", authUser);
+
         return result;
 
     }
 
     @ApiOperation(value = "门户信息修改接口")
     @PostMapping("changeportal/{changetype}")
-    public Map<String, Object> changeportal(@PathVariable("changetype") String changetype, @RequestParam("username") String username, @RequestParam("changecontent") String changecontent){
-        HashMap<String, Object> result = new HashMap<>();
-        AuthUser authUser = authUserRepository.findAuthUserByUsername(username);
+    public Result changeportal(@PathVariable("changetype") String changetype, @RequestParam("username") String username, @RequestParam("changecontent") String changecontent){
+        AuthUser authUser = authUserService.getAuthUserByUsername(username);
+
+        Result result = new Result();
         if(changetype.equals("researchfield")){
             authUser.setResearchfield(changecontent);
-            result.put("code",201);
-            result.put("msg","门户信息中的研究领域修改成功");
+            result.setMsg("门户信息中的研究领域修改成功");
+            result.setCode(201);
             return result;
         }
         else if(changetype.equals("realname")){
             authUser.setRealname(changecontent);
-            result.put("code",202);
-            result.put("msg","门户信息中的真实姓名 修改成功");
+            result.setMsg("门户信息中的真实姓名 修改成功");
+            result.setCode(202);
             return result;
         }
         else{
-            result.put("code",400);
-            result.put("msg","门户信息修改失败");
+            result.setMsg("门户信息修改失败");
+            result.setCode(400);
             return result;
         }
     }
