0324b4f4b0d2fead4265fcad03fb378ba195bd9a
wjxchn <1822630963@qq.com>
2020-12-17 21:00:34 +0800

    new

--- jingzhou/src/main/java/jingzhou/MySQLTable/Patent.java
+++ jingzhou/src/main/java/jingzhou/MySQLTable/Patent.java
@@ -13,26 +13,36 @@ public class Patent {
     //专利ID
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer patentid;
-    //发明人
-    String inventor;
-    //申请公布日
-    Date pubdate;
     //专利名称
     String patentname;
-    //申请公布号
-    String pubnumber;
-    //申请号
+    //专利类型
+    String type;
+    //申请专利号
     String patentnumber;
+    //申请日
+    Date appdate;
+    //授权公布号
+    String pubnumber;
+    //授权公布日
+    Date pubdate;
     //申请人
     String applicant;
     //地址
     String address;
+    //发明人
+    String inventor;
     //分类号
     String classification;
+    //主分类号
+    String mainclassification;
+    //国省代码
+    String provincecode;
+    //代理机构
+    String agentinstitution;
+    //代理人
+    String agentpeople;
+    //主权项
+    String force;
     //摘要
     String abstractcontent;
-    //申请日
-    Date appdate;
-
-
 }
