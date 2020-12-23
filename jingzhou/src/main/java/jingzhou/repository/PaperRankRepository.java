b5aba7e6894614e4e141cf117c91a0f66c9ec04b
NorthanX <786260621@qq.com>
2020-12-23 22:54:39 +0800

    纠错

--- jingzhou/src/main/java/jingzhou/repository/PaperRankRepository.java
+++ jingzhou/src/main/java/jingzhou/repository/PaperRankRepository.java
@@ -4,7 +4,9 @@ import jingzhou.MySQLTable.PaperRank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaperRankRepository extends JpaRepository<PaperRank, Integer> {
-
+    List<PaperRank> findAll();
 }
